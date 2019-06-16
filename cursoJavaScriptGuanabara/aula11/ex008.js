f04ec020386113c2e3088512edf64a17da6d0c82
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-06-16 00:03:39 -0300

    Curso Guanabara - Javascript - Aula 11 - Condições (Parte 1)

---
+++ cursoJavaScriptGuanabara/aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log("O console funcionar corretamente!");
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel} Km/h`);
+if(vel > 60) { //Condição Simples
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}
+
+console.log("Digija sempre usando cinto de segurança");
---
+++ cursoJavaScriptGuanabara/aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = "França";
+console.log(`Vivendo em ${pais}`);
+//if(pais == "Brasil"){
+if (pais != "Brasil"){
+    //console.log("Você é Brasileiro");
+    console.log("Você é Estrangeiro");
+} else {
+    //console.log("Você é Estrangeiro");
+    console.log("Você é Brasileiro");
+}
