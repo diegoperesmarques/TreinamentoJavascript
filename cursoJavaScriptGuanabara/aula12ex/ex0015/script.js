3bd7d313b35905faba95950371363a6be60c8882
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-06-20 23:01:32 -0300

    Curso Guanabara - JavaScript - Exercícios JavaScript (Parte 3)

---
+++ cursoJavaScriptGuanabara/aula12ex/ex0015/script.js
@@ -0,0 +1,51 @@
+function verificar(){
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById("txtano");
+    var res = document.querySelector("div#res");
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert("[ERRO]! Verifique os dados e tente novamente!");
+    } else {
+        var fsex = document.getElementsByName("radsex");
+        var idade = ano - Number(fano.value);
+        var genero = "";
+        var img = document.createElement("img");
+        img.setAttribute("id", "foto");
+        if (fsex[0].checked){
+            genero = "Homem";
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute("src", "criancahomem.jpg");
+            } else if (idade < 21){
+                // Jovem
+                img.setAttribute("src", "jovemhomem.jpg");
+            } else if (idade < 50){
+                // Adulto
+                img.setAttribute("src", "adultohomem.jpg");
+            } else {
+                // Idoso
+                img.setAttribute("src", "idosohomem.jpg");
+            }
+
+        } else {
+            genero = "Mulher";
+            if(idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute("src", "criancamulher.jpg");
+            } else if (idade < 21){
+                // Jovem
+                img.setAttribute("src", "jovemmulher.jpg");
+            } else if (idade < 50){
+                // Adulto
+                img.setAttribute("src", "adultomulher.jpg");
+            } else {
+                // Idoso
+                img.setAttribute("src", "idosomulher.jpg");
+            }
+
+        }
+        res.style.textAlign = "center";
+        res.innerHTML = `Dectamos ${genero} com ${idade} anos.`;
+        res.appendChild(img);
+    }
+}
