91401c8d908ed9cabbd7380785b3de5765faac0c
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-06-24 20:08:41 -0300

    Curso Rocketseat - JavaScript - Excluindo todos

--- ROCKETSEAT/JavaScript/app/todos.js
+++ ROCKETSEAT/JavaScript/app/todos.js
@@ -15,7 +15,22 @@ function renderTodos (){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' +pos+ ')');
+
+        var linkText = document.createTextNode('Excluir');
+
+
+        linkElement.appendChild(linkText);
+
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
     }
 }
@@ -31,3 +46,9 @@ function addTodo(){
 }
 
 buttonElement.onclick = addTodo;
+
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+}
