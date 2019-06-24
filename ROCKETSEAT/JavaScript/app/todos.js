991ea8c30c5cafc6ab5f741dd5a4b7f544703df8
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-06-24 19:17:42 -0300

    Curso Rocketseat - JavaScript - Iniciando aplicação

---
+++ ROCKETSEAT/JavaScript/app/todos.js
@@ -0,0 +1,9 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonelement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketset'
+];
