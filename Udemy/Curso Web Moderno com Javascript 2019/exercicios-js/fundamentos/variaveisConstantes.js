8ce9fb51d014a4538a0752ba09f450d4fcf17599
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-06-29 22:19:51 -0300

    Udemy - Curso Web Moderno 2019 - 16.O Básico de Var, Let e Const

---
+++ Udemy/Curso Web Moderno com Javascript 2019/exercicios-js/fundamentos/variaveisConstantes.js
@@ -0,0 +1,18 @@
+
+// tipo da variável, identificador e valor
+var a = 3;
+let b = 4;
+
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+
+console.log(a, b);
+
+const c = 5;
+console.log(c);
