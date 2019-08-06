0cb74e8b2cfb4f1b2f845defa8235570984401de
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-08-06 20:54:58 -0300

    197. Seletores CSS #02 - Seletor Geral

---
+++ Udemy/Curso Web Moderno com Javascript 2019/css/css/js/tag.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.tag').forEach(box => {
+    const tagName = box.tagName.toLowerCase();
+
+    box.style.borderColor = '#616161';
+
+    if (!box.classList.contains('nolabel')){
+        const label = document.createElement('label');
+        
+    }
+})
