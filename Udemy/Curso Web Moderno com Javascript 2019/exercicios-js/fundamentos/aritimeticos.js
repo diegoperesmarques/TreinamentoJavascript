1a4f5fa020e531e8317bff04a8fc1a85bdfda439
Diego Peres marques <diegoperesmarques@hotmail.com>
2019-09-13 20:56:59 -0300

    Udemy - Curso Web completo 2019 - 45. Operadores: Aritméticos

---
+++ Udemy/Curso Web Moderno com Javascript 2019/exercicios-js/fundamentos/aritimeticos.js
@@ -0,0 +1,7 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 /* Muito utilizado para saber se o número é par ou impar */
+console.log(soma, subtracao, multiplicacao, divisao, modulo); 
